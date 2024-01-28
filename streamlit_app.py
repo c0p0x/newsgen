f2d5bcd9142cf3f91dd7af0f6b869e9331c71c01
c0p0x <c0p0x@users.noreply.github.com>
2024-01-28 21:47:07 +0100

    fd

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from langchain.chat_models import ChatOpenAI
 
+
+model = -1
+
+def set_model(model):
+    os.environ['OPENAI_MODEL'] = model
+def get_model():
+    return os.getenv('OPENAI_MODEL')
+
 def get_openai_api_key():
     # Retrieve the API key from an environment variable
     return os.getenv('OPENAI_API_KEY')
@@ -32,7 +40,7 @@ def get_latest_results(query):
     article_texts = []  # list to store original article texts for similarity comparison
 
     # Initialize the text_splitter before using it
-    text_splitter = TokenTextSplitter(chunk_size=3000, chunk_overlap=200)
+    text_splitter = TokenTextSplitter(chunk_size=2000, chunk_overlap=500)
 
     #iterate over each URL 
   
@@ -58,6 +66,8 @@ def get_latest_results(query):
     except ArticleException: 
         print(f"Failed to download and parse article: {query}")
 
+       
+
     return parsed_texts
 
 #required by chain.run()
@@ -74,15 +84,17 @@ def summarize_text(to_summarize_texts, openai_api_key):
     # set api key in env variable 
     set_openai_api_key(openai_api_key)
 
-    llm = ChatOpenAI(model_name='gpt-4-0125-preview', openai_api_key=openai_api_key, temperature=0.68, max_tokens = 3000)
+    llm = ChatOpenAI(model_name=get_model(), openai_api_key=openai_api_key, temperature=0.68, max_tokens = 3000)
     # Initialize the chain for summarization
     chain_summarize = load_summarize_chain(llm, chain_type="stuff")
     
     # Define prompt that generates titles for summarized text
     title_prompt = PromptTemplate(
-            input_variables=["text"], 
-            template="""Write an appropriate, clickbaity, but not judgemental news article title in Polish for less then approximatetly 200 characters for this text: {text}. Make sure it is in Polish and less then 100chars. Prepare list of 5 titles so I can choose. 
-            PROPOSED TITLES IN POLISH:"""
+        input_variables=["text"], 
+        template="""Write an appropriate, clickbaity, but not judgemental news article title in Polish for less then approximatetly 200 characters for this text: {text}. Make sure it is in Polish and less then 100chars. Prepare list of 5 titles so I can choose. 
+        
+        PROPOSED TITLES IN POLISH:
+        """
         )
     # define prompt that generates text translated 
     text_prompt = PromptTemplate(
@@ -90,6 +102,21 @@ def summarize_text(to_summarize_texts, openai_api_key):
         template="""Please provide engaging post of the following text in Polish, ensuring that it is 220 words approximate - SUPER IMPORTANT. The summary should be informative, neutral, and devoid of any judgmental tones focusing on and quoting facts from article. Remember, the post must be in Polish. {text}
         
         LONG SUMMARY IN POLISH:
+        """
+    )
+
+    facts_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Please provide a list of 10-15 key facts in polish - ONLY KEY FACTS - such as statistics, numbers, prices etc from the {text}
+        
+        LIST OF KEY FACTS IN POLISH:
+        """
+    )
+    full_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Make a fact-driven article in Polish from the following text: {text}
+        
+        LONG ARTICLE IN POLISH WITH FACTS AND QUOTES:
         """
     )
 
@@ -103,57 +130,83 @@ def summarize_text(to_summarize_texts, openai_api_key):
         
         # Summarize chunks here
         summarized_text = chain_summarize.run(to_summarize_text)
-
+        
         # prompt template that generates unique titles
         chain_prompt_title = LLMChain(llm=llm, prompt=title_prompt)
         clickbait_title = chain_prompt_title.run(summarized_text)
 
-        chain_prompt_text = LLMChain(llm=llm, prompt=text_prompt)
-        article = chain_prompt_text.run(summarized_text)
+       # full article parser
+        if to_summarize_texts and to_summarize_texts[0] and to_summarize_texts[0][0]:
+            desired_text = to_summarize_texts[0][0][0]
+            #st.write(desired_text)
+        else:
+             st.write("Text not found")
 
-        summarized_texts_titles_urls.append((clickbait_title, article, summarized_text, url))
+        
+        chain_prompt_text = LLMChain(llm=llm, prompt=text_prompt)
+        short_article = chain_prompt_text.run(summarized_text)
+
+        chain_prompt_text = LLMChain(llm=llm, prompt=full_prompt)
+        full_article = chain_prompt_text.run(desired_text)
+
+        chain_prompt_text = LLMChain(llm=llm, prompt=facts_prompt)
+        facts = chain_prompt_text.run(desired_text)
+
+        summarized_texts_titles_urls.append((clickbait_title, short_article, full_article, facts, summarized_text, url))
 
 
 
     return summarized_texts_titles_urls
 
 
-
-
 def main():
-    #frontend
-    st.title('AutoNewsletter')
+    st.title('AutoNewsletter-DEV')
 
-    #create text input field for API keys 
+    # Create text input field for API keys 
     openai_api_key = st.text_input("Insert your OpenAI api key: ", type="password")
 
- 
-    #create text input field for keyword 
-    user_query = st.text_input("URL")
+    selectbox = st.selectbox("GPT Model to be used", ("gpt-3.5-turbo-1106", "gpt-4-1106-preview", "gpt-4-0125-preview"))
+    if selectbox == "gpt-3.5-turbo-1106":
+        set_model("gpt-3.5-turbo-1106")
+    elif selectbox == "gpt-4-1106-preview":
+        set_model("gpt-4-1106-preview")
+    elif selectbox == "gpt-4-0125-preview":
+        set_model("gpt-4-0125-preview")   
+    selectbox = st.selectbox("Raw text or URL source", ("URL", "Raw text"))
 
-    if st.button('Submit'):
-        st.session_state.user_query = user_query
+    if selectbox == "Raw text":
+        raw_text = st.text_area(label="Text", height=300, max_chars=10000)
+        if st.button("Submit Raw Text"):
+            summarize(raw_text)
+            if st.session_state.summary:
+                st.text_area(
+                    label="Raw text summary",
+                    value=st.session_state.summary,
+                    height=100,
+                )
+                logging.info(f"Text: {raw_text}\nSummary: {st.session_state.summary}")
 
-        # Split the result of get_latest_results into two separate variables
-        st.session_state.get_splitted_text = get_latest_results(user_query)
-        if not st.session_state.get_splitted_text:
-            st.write("No results found.")
-        st.session_state.summarized_texts = summarize_text(st.session_state.get_splitted_text, openai_api_key)
-        
-        for title, article, summarized_text, url in st.session_state.summarized_texts:
-          st.markdown("## Suggested titles") 
-          st.write(title)
-          # Add the emoji before the summarized text
-          st.markdown("## Suggested articles") 
-          st.write(f"❇️ {article}")
-          st.write(f"🔗 {url}")
-          # Create an empty line for a gap
-          st.markdown("\n\n")
+    elif selectbox == "URL":
+        user_query = st.text_input(label="URL")
+        if st.button("Submit URL"):
+            st.session_state.user_query = user_query
+            st.session_state.get_splitted_text = get_latest_results(user_query)
+            st.session_state.summarized_texts = summarize_text(st.session_state.get_splitted_text, openai_api_key)
+            if st.session_state.get_splitted_text:
+                for title, short_article, full_article, facts, summarized_text, url in st.session_state.summarized_texts:
+                    st.markdown("## Headline") 
+                    st.write(title)
+                    st.markdown("## Summary") 
+                    st.write(f"❇️ {short_article}")
+                    st.markdown("## Key facts") 
+                    st.write(facts)
+                    st.markdown("## Full article") 
+                    st.write(full_article)
+                    st.write(f"🔗 {url}")
+                    st.markdown("\n\n")
 
-    # wipe api key 
+    # Wipe API key 
     clear_openai_api_key()
-
-    return openai_api_key
 
 
 if __name__ == "__main__":
