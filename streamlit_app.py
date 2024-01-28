3d1b0c11ef9a9ec7d336c06cfb0c4aa996783ace
c0p0x <c0p0x@users.noreply.github.com>
2024-01-28 22:12:48 +0100

    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,6 +158,84 @@ def summarize_text(to_summarize_texts, openai_api_key):
 
     return summarized_texts_titles_urls
 
+def summarize_text_raw_text(to_summarize_texts, openai_api_key):
+  
+    summarized_texts_titles_urls = []
+
+    # set api key in env variable 
+    set_openai_api_key(openai_api_key)
+
+    llm = ChatOpenAI(model_name=get_model(), openai_api_key=openai_api_key, temperature=0.68, max_tokens = 3000)
+    # Initialize the chain for summarization
+    chain_summarize = load_summarize_chain(llm, chain_type="stuff")
+    
+    # Define prompt that generates titles for summarized text
+    title_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Write an appropriate, clickbaity, but not judgemental news article title in Polish for less then approximatetly 200 characters for this text: {text}. Make sure it is in Polish and less then 100chars. Prepare list of 5 titles so I can choose. 
+        
+        PROPOSED TITLES IN POLISH:
+        """
+        )
+    # define prompt that generates text translated 
+    text_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Please provide engaging post of the following text in Polish, ensuring that it is 220 words approximate - SUPER IMPORTANT. The summary should be informative, neutral, and devoid of any judgmental tones focusing on and quoting facts from article. Remember, the post must be in Polish. {text}
+        
+        LONG SUMMARY IN POLISH:
+        """
+    )
+
+    facts_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Please provide a list of 10-15 key facts in polish - ONLY KEY FACTS - such as statistics, numbers, prices etc from the {text}
+        
+        LIST OF KEY FACTS IN POLISH:
+        """
+    )
+    full_prompt = PromptTemplate(
+        input_variables=["text"], 
+        template="""Make a fact-driven article in Polish from the following text: {text}
+        
+        LONG ARTICLE IN POLISH WITH FACTS AND QUOTES:
+        """
+    )
+
+    
+    # Convert each text string to a Document object
+    to_summarize_text = [Document('Dummy Title', text) for text in to_summarize_text]
+        
+    if not to_summarize_text:  # Check if list is empty before running the chain
+        print(f"No text to summarize for URL: {url}")
+        continue
+        
+    # Summarize chunks here
+    summarized_text = chain_summarize.run(to_summarize_text)
+        
+    # prompt template that generates unique titles
+    chain_prompt_title = LLMChain(llm=llm, prompt=title_prompt)
+    clickbait_title = chain_prompt_title.run(summarized_text)
+
+    # full article parser
+    if to_summarize_texts and to_summarize_texts[0] and to_summarize_texts[0][0]:
+        desired_text = to_summarize_texts[0][0][0]
+        #st.write(desired_text)
+    else:
+        st.write("Text not found")
+
+        
+    chain_prompt_text = LLMChain(llm=llm, prompt=text_prompt)
+    short_article = chain_prompt_text.run(summarized_text)
+
+    chain_prompt_text = LLMChain(llm=llm, prompt=full_prompt)
+    full_article = chain_prompt_text.run(desired_text)
+
+    chain_prompt_text = LLMChain(llm=llm, prompt=facts_prompt)
+    facts = chain_prompt_text.run(desired_text)
+
+    summarized_texts_titles_urls.append((clickbait_title, short_article, full_article, facts, summarized_text, url))
+
+    return summarized_texts_titles_urls
 
 def main():
     st.title('AutoNewsletter-DEV')
@@ -177,7 +255,7 @@ def main():
     if selectbox == "Raw text":
         raw_text = st.text_area(label="Text", height=300, max_chars=10000)
         if st.button("Submit Raw Text"):
-            st.session_state.summarized_texts = summarize_text(raw_text, openai_api_key)
+            st.session_state.summarized_texts = summarize_text_raw_text(raw_text, openai_api_key)
             if st.session_state.get_splitted_text:
                 for title, short_article, full_article, facts, summarized_text, url in st.session_state.summarized_texts:
                     st.markdown("## Headline") 
